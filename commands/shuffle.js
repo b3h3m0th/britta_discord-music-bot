f9b9068206ac7693d125c9991c3965209d198dbc
Behemoth <simonostini@gmail.com>
2020-11-28 13:40:33 +0100

    ⚡️ shuffle is working :D

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,11 +1,22 @@
+const { MessageEmbed } = require("discord.js");
 const { canModifyQueue } = require("../util/shuffleUtil");
+const language = require("../languages/english");
 
 module.exports = {
   name: "shuffle",
   description: "Shuffle queue",
+  aliases: ["sh"],
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is no queue.").catch(console.error);
+    if (!queue)
+      return message.channel.send(
+        new MessageEmbed()
+          .setAuthor(
+            language("error").nothing_music,
+            message.author.avatarURL()
+          )
+          .setColor(message.client.config.colors.failed)
+      );
     if (!canModifyQueue(message.member)) return;
 
     let songs = queue.songs;
@@ -15,6 +26,15 @@ module.exports = {
     }
     queue.songs = songs;
     message.client.queue.set(message.guild.id, queue);
-    queue.textChannel.send(`${message.author} 🔀 shuffled the queue`).catch(console.error);
-  }
+    queue.textChannel
+      .send(
+        new MessageEmbed()
+          .setAuthor(
+            `🔀 ${message.author.username} shuffled te queue`,
+            message.author.avatarURL()
+          )
+          .setColor(message.client.config.colors.primary)
+      )
+      .catch(console.error);
+  },
 };
