974f1e2edbcb2796e518a7a287abd7b8474f4627
Behemoth <simonostini@gmail.com>
2020-12-06 18:14:49 +0100

    ✨ introducing cat command

---
+++ commands/cat.js
@@ -0,0 +1,29 @@
+global.fetch = require("node-fetch");
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+const config = require("../config");
+const {
+  commands: { categories },
+} = require("../config");
+
+module.exports = {
+  name: "cat",
+  cooldown: 3,
+  categories: [categories.fun],
+  usages: [""],
+  examples: [""],
+  description: "Sends a cat image",
+  execute: async (message) => {
+    try {
+      let catRes = await message.client.alexclient.image.cats();
+      return message.channel.send(
+        new MessageEmbed()
+          .setTimestamp()
+          .setAuthor("😺 Here's a cute cat for Anika")
+          .setImage(catRes.file)
+          .setColor(config.colors.primary)
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
