bc08ed6ebb299a91a2ea29b37c7e95d4499fe9a7
Behemoth <simonostini@gmail.com>
2020-12-21 13:01:07 +0100

    🐛 bug fix join command

--- commands/join.js
+++ commands/join.js
@@ -15,11 +15,28 @@ module.exports = {
     const language = require(`../languages/${thisLang}`);
 
     const { channel } = message.member.voice;
+    console.log(message.guild.voice);
     if (channel) {
-      await channel.join();
-      return new MessageEmbed()
-        .setColor(config.colors.primary)
-        .setAuthor("✔️ Hey I joined your channel", message.author.avatarURL());
+      if (message.guild.voice && message.guild.voice.channelID !== channel.id) {
+        await channel.join();
+        return message.channel.send(
+          new MessageEmbed()
+            .setColor(config.colors.primary)
+            .setAuthor(
+              "✔️ Hey! I joined your channel",
+              message.author.avatarURL()
+            )
+        );
+      } else {
+        message.channel.send(
+          new MessageEmbed()
+            .setAuthor(
+              "✔️ I already joined your channel",
+              message.author.avatarURL()
+            )
+            .setColor(config.colors.primary)
+        );
+      }
     } else {
       return message.channel
         .send(
