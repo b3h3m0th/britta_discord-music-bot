2e8f862b50a593b3910edc500888e7e2e54b2074
Behemoth <simonostini@gmail.com>
2021-01-17 12:49:16 +0100

    🔥 added config.example.ts

---
+++ src/config.example.ts
@@ -0,0 +1,70 @@
+module.exports = {
+  client: {
+    name: "",
+    prefix: "",
+    token: "",
+    invite_link: "",
+    top_gg_vote_link: "",
+    donate_link: "",
+  },
+  commands: {
+    categories: {
+      music: "🎵  Music",
+      fun: "🤪  Fun",
+      info: "ℹ️  Info",
+      config: "⚙️  Config",
+      owner: "👑 Owner only",
+      premium: "🔒 Premium (Vote locked)",
+    },
+  },
+  dev: {
+    behemoth: {
+      name: "Behemoth",
+      discriminator: "4026",
+      id: "491539179642028032",
+    },
+  },
+  colors: {
+    primary: "#e97e37",
+    failed: "#e74c3c",
+    warning: "#e67e22",
+  },
+  api: {
+    youtube_key: "",
+    spotify_client_id: "",
+    spotify_client_secret: "",
+    spotify_redirect_uri: "",
+    soundcloud_client_id: "",
+    alexflipnote_token: "",
+    top_gg_token: "",
+  },
+  resources: {
+    youtubeIcon:
+      "http://pluspng.com/img-png/youtube-play-button-transparent-png-image-42015-800.png",
+    spotifyIcon: "http://pluspng.com/img-png/spotify-logo-png-open-2000.png",
+    twitchIcon:
+      "https://cdn3.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-twitch-circle-512.png",
+    soundcloudIcon:
+      "https://cdn2.iconfinder.com/data/icons/minimalism/512/soundcloud.png",
+    coinflip:
+      "https://cdn.discordapp.com/attachments/738089927925039206/738092778520707172/animation_500_kd7o8hyq.gif",
+    now_playing:
+      "https://images-ext-1.discordapp.net/external/9_3dcPqCXGMU3WFySOvtVYjIKsZnN6zcyg7oVTn8Zlw/%3Fv%3D1/https/cdn.discordapp.com/emojis/673357192203599904.gif",
+    geniusIcon:
+      "https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2F055925d5f3d032c7aadc468779ed2b31.512x512x1.jpg",
+  },
+  support: {
+    server: {
+      invite_link: "",
+    },
+  },
+  database: {
+    mongodb_connection_string: "",
+  },
+  lavalink: {
+    host: "",
+    password: "",
+    port: 0,
+  },
+  inDevelopment: true,
+};
