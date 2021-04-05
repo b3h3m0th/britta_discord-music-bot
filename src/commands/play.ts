9ce3582a7b5e301ae8939d6b55e6fbcedb29e68d
Behemoth <simonostini@gmail.com>
2021-04-05 12:09:04 +0200

    🎨 update of lavalink

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -192,7 +192,7 @@ module.exports = class Play extends Command {
       }
     } else {
       const searchQuery = {
-        source: "soundcloud",
+        source: "youtube",
         query: args.slice(0).join(" "),
       };
       loadTracks(
