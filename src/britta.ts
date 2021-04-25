91f2bff1c9651612839737842b3bdf89711e31ca
Behemoth <54234287+b3h3m0th@users.noreply.github.com>
2021-04-25 09:39:54 +0200

    Update britta.ts

--- src/britta.ts
+++ src/britta.ts
@@ -3,7 +3,7 @@ import Britta from "./structures/Client";
 const client = new Britta(config.client.token);
 
 client.init();
-
+ 
 ["events", "commands", "filters"].forEach((handler) => {
   require(`./handlers/${handler}`)(client);
 });
